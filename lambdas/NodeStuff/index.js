8439281f7e2e225b57c57f9c3b96cd11982c3d45
Phu Lam Nguyen <mtlplnguyen@gmail.com>
2022-01-14 12:33:08 -0500

    v2

--- lambdas/NodeStuff/index.js
+++ lambdas/NodeStuff/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         ...event.Payload,
-        nodeProperty: "Added a new property via the Node Lambda"
+        nodeProperty: "Added a new property via the Node Lambda v2"
     };
     
     return response;
